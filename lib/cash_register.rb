7458e02940f42ed808e062ac3a5a769608930f8a
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-14 07:47:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,43 +1,48 @@
+class CashRegister 
+  attr_accessor :discount, :total
 
-class CashRegister
-
-  attr_accessor :total, :discount, :last_transaction_amount, :items
-
-  def initialize(discount = 0)
+  def initialize(discount=0)
+    @discount = discount 
     @total = 0
-    @discount = discount
     @items = []
-  end
-
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end
-
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
+    @last_item = [] 
+  end 
+  
+  def apply_discount
+    if discount != 0 
+      @total -= (@total * (@discount / 100.to_f))
       "After the discount, the total comes to $#{@total.to_i}."
-    else
+    else 
       "There is no discount to apply."
-    end
-  end
+    end 
+  end 
+  
+  def add_item(title, price, quantity=1)
+    @total += price * quantity 
+    quantity.times do 
+      @items << title 
+    end 
+    @last_item = price * quantity 
+  end 
+  
+  def items 
+    @items 
+  end 
+  
+  def void_last_transaction 
+     @items.delete_at(-1)
+    self.total = self.total - @last_item
+  end 
+end 
 
-
-  def void_last_transaction()
-    @total -= @last_transaction_amount
-  end
-
-end
+list = CashRegister.new(20) #=>#<CashRegister:0x005616084063b0 @discount=20, @total=0>
+list.add_item("eggs", 1.50, 3) #=> 4.50
+list.add_item("skirt steak", 7.99) #=> 7.99 
+list.add_item("apple pie", 3.99, 2) #=> 7.98
+list.total #=> 20.47
+list.items #=> ["eggs", "eggs", "eggs", "skirt steak", "apple pie", "apple pie"]
+list.apply_discount #=> "After the discount, the total comes to $16."
+list.void_last_transaction
+list.items  #=> ["eggs", "eggs", "eggs", "skirt steak", "apple pie"]
+list.total #=> 8.02
+list #=> 
